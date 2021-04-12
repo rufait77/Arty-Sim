d853e130dcb951a1ab708c12da125b9ee97cd11e
rufait77 <81951354+rufait77@users.noreply.github.com>
2021-04-12 13:18:35 +0600

    Add files via upload

---
+++ CannonBall.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour {
+
+
+	public float lifeTime = 5f;
+	public GameObject explosion;
+	public float minY = -20f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		StatusCheck();
+	}
+
+		
+
+	void StatusCheck() {
+
+		lifeTime -= Time.deltaTime;
+
+		if (lifeTime < 0){
+
+			Destroy();
+		}
+
+		if (transform.position.y < minY){
+
+
+			Destroy();
+		}
+
+		
+	}
+
+	void Destroy() {
+
+		Instantiate(explosion, transform.position, transform.rotation);
+		Destroy(this.gameObject);
+	}
+}
